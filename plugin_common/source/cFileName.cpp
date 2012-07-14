98698df8cab4d0a0b8bf27839abafd0db6cc0f1c
Frank Stain <dr.frank.stain@gmail.com>
2012-07-15 02:09:59 +0700

    [*] relative path

--- plugin_common/source/cFileName.cpp
+++ plugin_common/source/cFileName.cpp
@@ -1,22 +1,10 @@
 #include "plugin_common.h"
 #include "cFileName.h"
+#include <shlwapi.h>
+
+#pragma comment( lib, "shlwapi.lib" )
 
 const string cFileName::PATH_SEPARATOR = "\\";
-
-class cPathList {
-private:
-	string			m_volume;
-	list<string>	m_dirs;
-
-public:
-	cPathList() : m_volume("") {};
-	cPathList( const string& path );
-
-	string path() const;
-
-	const string& volume() const { return m_volume; };
-	list<string>& dirs() { return m_dirs; };
-};
 
 cFileName::cFileName( const string& name ){
 	explode_path( name );
@@ -50,74 +38,10 @@ void cFileName::explode_path( const string& path ){
 
 cFileName cFileName::relative_path( const cFileName& base_path ) const {
 	cFileName result( *this );
-	cPathList my_dirs( m_path );
-	cPathList base_dirs( base_path.m_path );
-
-	if( my_dirs.volume() != base_dirs.volume() ){
-		return result;
-	};
-
-	cPathList relative_path;
-	list<string>::iterator my_dir	= my_dirs.dirs().begin();
-	list<string>::iterator base_dir	= base_dirs.dirs().begin();
-	while( ( my_dirs.dirs().end() != my_dir ) && ( base_dirs.dirs().end() != base_dir ) ){
-		if( my_dir != base_dir ){
-			while( base_dirs.dirs().end() != base_dir ){
-				relative_path.dirs().push_back( ".." );
-				base_dir++;
-			};
-
-			while( my_dirs.dirs().end() != my_dir ){
-				relative_path.dirs().push_back( *my_dir );
-				my_dir++;
-			};
-
-			result.m_path = relative_path.path();
-			return result;
-		};
-
-		my_dir++;
-		base_dir++;
-	};
-
-	while( my_dirs.dirs().end() != my_dir ){
-		relative_path.dirs().push_back( *my_dir );
-		my_dir++;
-	};
-
-	result.m_path = relative_path.path();
-	return result;
-};
-
-cPathList::cPathList( const string& path ){
-	const char psep = *cFileName::PATH_SEPARATOR.c_str();
-	size_t pos = path.find( ":" );
-	size_t pos2;
-	if( string::npos == pos ){
-		m_volume	= "";
-		pos2		= path.find( psep );
-		pos			= 0;
-	}else{
-		m_volume = path.substr( 0, pos );
-		pos += 2;
-		pos2 = path.find( psep, pos );
-	};
 	
-	while( string::npos != pos2 ){
-		m_dirs.push_back( path.substr( pos, pos2 - pos ) );
-		pos = pos2 + 1;
-		pos2 = path.find( psep, pos );
-	};
-
-	if( pos ){
-		m_dirs.push_back( path.substr( pos, string::npos ) );
-	};
-};
-
-string cPathList::path() const {
-	string result = ( m_volume.size() )? m_volume + ":" + cFileName::PATH_SEPARATOR : "";
-	for( list<string>::const_iterator fd = m_dirs.begin(); m_dirs.end() != fd; fd++ ){
-		result += *fd + cFileName::PATH_SEPARATOR;
+	char relative_path[ MAX_PATH ];
+	if( PathRelativePathTo( relative_path, m_path.c_str(), FILE_ATTRIBUTE_DIRECTORY, base_path.path().c_str(), FILE_ATTRIBUTE_DIRECTORY ) ){
+		result.m_path = relative_path;
 	};
 
 	return result;
